4b51f3dedb2c3948c25af11cb9e4ba7107ef9af0
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2021-08-11 15:19:18 -0300

    Projeto em andamento

---
+++ listadetarefas/android/app/src/main/kotlin/br/com/listadetarefas/listadetarefas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.listadetarefas.listadetarefas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ listadetarefas/lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _toDolist = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        title: Text('Lista de tarefas'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Nova tarefa',
+                        labelStyle: TextStyle(color: Colors.blueAccent),
+                      ),
+                    ),),
+                  ElevatedButton(onPressed: (){}, child: Text('Add'))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDolist);
+    final file = await _getFile();
+    return file.writeAsString(data);
+
+  }
+
+  Future<String> _readData() async {
+      try {
+        final file = await _getFile();
+        return file.readAsString();
+      } catch (e) {
+        return "Erro";
+      }
+  }
+}
+
+
+
